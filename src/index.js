6dfff3ffbb1b0191d376e24f0b670cf70772f95a
mumu2117 <jmillerarcher@me.comgit config --global user.name mumu2117>
2020-04-01 21:19:51 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById('create-task-form')
+const taskItemUl = document.getElementById('tasks')
+
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const taskInput = taskForm["new-task-description"].value
+
+  // const newLiElement = document.createElement('li')
+  // newLiElement.innerText = taskInput
+  // taskItemUl.appendChild(newLiElement)
+
+  taskItemUl.innerHTML += `<li id=${taskInput}>${taskInput}</li><button id=${taskInput}>X</button>`
+
+  taskForm["new-task-description"].value = ""
+
+});
+
+taskItemUl.addEventListener("click", (e) => {
+  //e.preventDefault()
+  clickMe(e);
+});
+
+function clickMe(e) {
+  if (e.target.tagName === "BUTTON") {
+    e.target.previousElementSibling.remove();
+    e.target.remove();
+  }
+}
 });
